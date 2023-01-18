a2bed56d3c5fff5565e93e7c50851434c06c7c71
satxnine <fnuraini503@gmail.com>
2023-01-18 14:34:43 +0700

    crud php mysqli ajax

---
+++ tambah-siswa.php
@@ -0,0 +1,155 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <title>Tambah Siswa</title>
+  </head>
+
+  <body>
+
+    <div class="container" style="margin-top: 80px">
+      <div class="row">
+        <div class="col-md-8 offset-md-2">
+          <div class="card">
+            <div class="card-header">
+              TAMBAH SISWA
+            </div>
+
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="index.php">Data Tabel</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Data</li>
+              </ol>
+            </nav>
+
+            <div class="card-body">
+              <form action="s" method="POST">
+                
+                <div class="form-group">
+                  <label>NISN</label>
+                  <input type="text" name="nisn" placeholder="Masukkan NISN Siswa" class="form-control">
+                </div>
+
+                <div class="form-group">
+                  <label>Nama Lengkap</label>
+                  <input type="text" name="nama_lengkap" placeholder="Masukkan Nama Siswa" class="form-control">
+                </div>
+
+                <div class="form-group">
+                  <label>Alamat</label>
+                  <textarea class="form-control" name="alamat" placeholder="Masukkan Alamat Siswa" rows="4"></textarea>
+                </div>
+                
+                <button class="btn btn-login btn-success">SIMPAN</button>
+                <button class="btn btn-login btn-danger">RESET</button>
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+    <script>
+      $(document).ready(function() {
+
+        $(".btn-login").click( function() {
+        
+          var nisn = $("#nisn").val();
+          var nama_lengkap = $("#nama_lengkap").val();
+          var alamat = $("#alamat").val();
+
+          if(nisn.length == "") {
+
+            Swal.fire({
+              type: 'warning',
+              title: 'Oops...',
+              text: 'NISN Wajib Diisi !'
+            });
+
+          } else if(nama_lengkap.length == "") {
+
+            Swal.fire({
+              type: 'warning',
+              title: 'Oops...',
+              text: 'Nama Lengkap Wajib Diisi !'
+            });
+
+          } else if(alamat.length == "") {
+
+            Swal.fire({
+              type: 'warning',
+              title: 'Oops...',
+              text: 'Alamat Wajib Diisi !'
+            });
+
+          } else {
+
+            $.ajax({
+
+              url: "simpan-siswa.php",
+              type: "POST",
+              data: {
+                  "nisn": nisn,
+                  "nama_lengkap": nama_lengkap,
+                  "alamat": alamat
+              },
+
+              success:function(response){
+
+                if (response == "success") {
+
+                  Swal.fire({
+                    type: 'success',
+                    title: 'Berhasil Disimpan!',
+                    text: 'Anda akan di arahkan dalam 3 Detik',
+                    timer: 3000,
+                    showCancelButton: false,
+                    showConfirmButton: false
+                  })
+                  .then (function() {
+                    window.location.href = "dashboard.php";
+                  });
+
+                } else {
+
+                  Swal.fire({
+                    type: 'error',
+                    title: 'Gagal Disimpan!',
+                    text: 'silahkan coba lagi!'
+                  });
+
+                }
+
+                console.log(response);
+
+              },
+
+              error:function(response){
+
+                  Swal.fire({
+                    type: 'error',
+                    title: 'Opps!',
+                    text: 'server error!'
+                  });
+
+                  console.log(response);
+
+              }
+
+            });
+
+          }
+
+        });
+
+      });
+    </script>
+  </body>
+</html>
